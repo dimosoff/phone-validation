6a758f78b242ca7c7ee94b91f69e3a910d5c10d6
dimosoff <dimmiostr@mail.ru>
2022-11-27 16:47:54 +0300

    delete to make

--- script.js
+++ script.js
@@ -1,60 +1,86 @@
 const phoneElements = document.querySelectorAll("input[name=phone]");
 
-phoneElements.forEach((elem) =>
-  validatePhoneNumber(elem, elem.dataset.code, elem.dataset.mask)
-);
+phoneElements.forEach((elem) => validatePhoneNumber(elem));
 
-function validatePhoneNumber(input, inputCode, inputMask) {
-  const codeLength = +inputCode.length + 2;
-  let currentValue = "",
-    cursorPosition = 0;
-  const maskArray = phoneMaskArray(inputMask);
+function validatePhoneNumber(input) {
+  const inputMaskValue = input.dataset.mask,
+    maskArray = phoneMaskArray(inputMaskValue);
+
+  let currentValue = input.value,
+    cursorPosition = maskArray[0];
 
   input.addEventListener("click", () => {
-    if (input.value !== "") return;
-
-    setInputState(`${inputCode} ${inputMask}`, codeLength);
-    currentValue = input.value;
-  });
-  input.addEventListener("input", (event) => {
-    const hasNumbers = /\d+/g.test(event.data);
-    const isText = event.inputType === "insertText";
-    const isDeleting =
-      event.inputType === "deleteContentBackward" || "deleteContentForward";
-    const isRightPlace = maskArray.includes(cursorPosition - codeLength);
-    console.log(event, input.selectionStart, input.selectionEnd);
-    cursorPosition = input.selectionStart;
-
-    if (isText && hasNumbers && isRightPlace) {
-      setInputState(generateValue(cursorPosition, event), cursorPosition);
+    if (input.value === "") {
+      setInputValue(inputMaskValue);
+      setCursorState();
       currentValue = input.value;
-      return;
+    } else {
+      cursorPosition = input.selectionStart;
     }
-    if (isDeleting) {
-      return;
-    }
-    setInputState(currentValue, cursorPosition - 1);
   });
 
-  function generateValue(cursorPosition, event) {
-    const valueLength = event.data.length;
-    if (!valueLength) return event.value;
-    return (
-      input.value.slice(0, cursorPosition) +
-      input.value.slice(cursorPosition + valueLength)
-    );
+  input.addEventListener("input", (event) => {
+    const hasNumbers = /\d+/g.test(event.data),
+      isEnd = cursorPosition - 1 === maskArray[maskArray.length - 1];
+
+    console.info(isEnd, event, hasNumbers);
+    if (!hasNumbers) {
+      setInputValue(currentValue);
+      setCursorState(cursorPosition);
+      return;
+    }
+    if (hasNumbers) {
+      console.log("-- " + cursorPosition);
+      setInputValue(generateValue(cursorPosition, event));
+      setCursorState(cursorPosition);
+      console.log("++ " + cursorPosition);
+      return;
+    }
+  });
+
+  function generateValue(position, event) {
+    const data = event.data;
+    if (!data) return event.target.value;
+    const dataLength = data.length,
+      isText = event.inputType === "insertText",
+      isDeleting =
+        event.inputType === "deleteContentBackward" || "deleteContentForward";
+    let inputValueArray = currentValue.split("");
+
+    cursorPosition = nextRightValuePlace(position - 1);
+    if (isDeleting) {
+      // nothing
+    }
+
+    for (let i = 0; i < dataLength; i++) {
+      console.log(i, inputValueArray[cursorPosition], cursorPosition);
+      inputValueArray[cursorPosition] = data;
+      cursorPosition = nextRightValuePlace(position);
+      console.log(i, inputValueArray[cursorPosition], cursorPosition);
+    }
+
+    currentValue = inputValueArray.join("");
+    return currentValue;
   }
-  function setInputState(value, cursorPosition) {
+  function setCursorState(cursor = maskArray[0]) {
+    input.selectionStart = cursor;
+    input.selectionEnd = cursor;
+    cursorPosition = cursor;
+  }
+  function setInputValue(value) {
     input.value = value;
-    input.selectionStart = cursorPosition;
-    input.selectionEnd = cursorPosition;
   }
   function phoneMaskArray(value) {
     let maskArray = [];
     value.split("").forEach((letter, index) => {
-      if (/\s|-|\(|\)/g.test(letter)) return;
+      if (!/_/g.test(letter)) return;
       maskArray.push(index);
     });
     return maskArray;
+  }
+  function nextRightValuePlace(position) {
+    const lastPosition = maskArray[maskArray.length - 1];
+    if (position === lastPosition) return lastPosition + 1;
+    return maskArray.find((elem) => elem > position);
   }
 }
